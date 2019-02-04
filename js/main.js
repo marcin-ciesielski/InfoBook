5cfba84e9b57cae43fe140ba246d7a02f38aba79
Marcin Ciesielski <marcin.ciesielski@pw.edu.pl>
2019-02-04 14:31:45 +0100

    Dodanie div

--- js/main.js
+++ js/main.js
@@ -1,48 +1,39 @@
 var url = 'http://127.0.0.1:8000/book/';
-function onSuccess(date) {
-    //console.log(date);
-}
 
-$(function() {
+
+var $bookList = $('#books');
     $.ajax({
         url: url,
-        data: {},
-        type: "GET",
-        dataType: "json"
-    }).done(function(result) {
-        onSuccess(result);
-        var lastTr = document.querySelector('tr:last-child');
-        var newDiv = document.createElement("div");
-        var lastDiv = document.querySelector('div:last-child');
-        //console.log(td);
-        for(var i=0; i<result.length; i++){
-            var cloneTr = lastTr.cloneNode(true);
-            lastTr.parentElement.appendChild(cloneTr);
-            //cloneTr.children[0].innerText = result[i].id;
-            //cloneTr.children[1].innerText = result[i].author;
-            cloneTr.children[0].innerText = result[i].title;
-            lastTr.appendChild(newDiv);
-            //console.log(result[i].author, result[i].title);
-
+        method: 'GET'
+    }).done(function(response) {
+        for(var i= 0; i < response.length; i++) {
+            var $li = $('<li data-id="' + response[i].id + '">');
+            $li.text(response[i].title);
+            var $div = $('<div class="description hidden">');
+            $bookList.append($li);
+            //$li.append($del);
+            $div.insertAfter($li);
         }
-    });
-});
 
-// No jQuery example
-// @see https://blog.garstasio.com/you-dont-need-jquery/ajax/#sending-and-receiving-json
-// var xhr = new XMLHttpRequest();
-// xhr.open('GET', url);
-// xhr.setRequestHeader('Content-Type', 'application/json');
-// xhr.onload = function() {
-//     if (xhr.status === 200) {
-//         var result = JSON.parse(xhr.responseText);
-//         onSuccess(result);
-//     }
-//     else {
-//         alert('Request failed.  Returned status of ' + xhr.status);
-//     }
-// };
-// xhr.send();
-// for(field in object){
-//  object[field];
-// }
+
+    });
+
+    //Zad 4
+        $bookList.on('click', 'li', function(event) {
+            var id = $(this).attr('data-id');
+            var $desc = $(this).next();
+            $desc.toggleClass('hidden');
+
+            $.ajax({
+                url: url + id,
+                method: 'GET'
+            }).done(function(response) {
+                var $details = '<p><b>Autor:</b> ' + response.author + '</p>' +
+                               '<p><b>Gatunek:</b> ' + response.genre + '</p>' +
+                               '<p><b>Wydawca:</b> ' + response.publisher + '</p>' +
+                               '<p><b>ISBN:</b> ' + response.isbn + '</p>';
+                $desc.html($details);
+
+            })
+        });
+
