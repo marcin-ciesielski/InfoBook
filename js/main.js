5d1ae0c9d288186fa0b59e0d9bd0afb2ab5cd759
Marcin Ciesielski <marcin.ciesielski@pw.edu.pl>
2019-02-04 15:44:02 +0100

    Dodanie div

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ var $bookList = $('#books');
         for(var i= 0; i < response.length; i++) {
             var $li = $('<li data-id="' + response[i].id + '">');
             $li.text(response[i].title);
-            var $div = $('<div class="description hidden">');
+            var $div = $('<div class="alert alert-dismissible alert-secondary description hidden">');
             $bookList.append($li);
             $div.insertAfter($li);
         }
@@ -17,19 +17,19 @@ var $bookList = $('#books');
 
     //Zadanie 4
         $bookList.on('click', 'li', function() {
-            var id = $(this).attr('data-id');
+            //var id = $(this).attr('data-id');
             var $desc = $(this).next();
             $desc.toggleClass('hidden');
 
-            $.ajax({
-                url: url + id,
-                method: 'GET'
-            }).done(function(response) {
-                var $details = '<p><b>Autor:</b> ' + response.author + '</p>' +
-                                '<p><b>ISBN:</b> ' + response.isbn + '</p>' +
-                                '<p><b>Wydawca:</b> ' + response.publisher + '</p>' +
-                                '<p><b>Gatunek:</b> ' + response.genre + '</p>';
-                $desc.html($details);
-            })
+            // $.ajax({
+            //     url: url + id,
+            //     method: 'GET'
+            // }).done(function(response) {
+            //     var $details = '<p><b>Autor:</b> ' + response.author + '</p>' +
+            //                     '<p><b>ISBN:</b> ' + response.isbn + '</p>' +
+            //                     '<p><b>Wydawca:</b> ' + response.publisher + '</p>' +
+            //                     '<p><b>Gatunek:</b> ' + response.genre + '</p>';
+            //     $desc.html($details);
+            // })
         });
 
